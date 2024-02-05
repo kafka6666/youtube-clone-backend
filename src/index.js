74baf6109ea1d494c51a7bc2e55e2becbc7c0742
Md. Abdullahel Kafi <md.abdullahelkafi@MAKs-MacBook-Pro.local>
2024-02-05 18:53:50 +0600

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB();
+
+// const app = express();
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     app.on("error", (error) => {
+//       console.log("ERROR: ", error);
+//       throw error;
+//     });
+
+//     app.listen(process.env.PORT, () => {
+//       console.log(`Our app is listening at Port ${process.env.PORT}`);
+//     });
+//   } catch (error) {
+//     console.error("ERROR: ", error);
+//     throw error;
+//   }
+// })();
